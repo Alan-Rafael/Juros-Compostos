90feaa0ec9ef2f216b7ceb7a36dffe361add0500
Weslen Bessa <weslen.bessa@ifood.com.br>
2025-01-22 17:58:55 -0300

    Commit para atualizar estrutura e nome de aquivos da pasta src

---
+++ src/calculo.js
@@ -0,0 +1,29 @@
+
+function calcularJurosComposto(valor, taxaAoMes, tempo_em_meses ){
+    let taxa = taxaAoMes/100;
+
+    let valorFinal = valor * Math.pow((1+taxa), tempo_em_meses)
+    
+    return valorFinal.toFixed(2);
+}
+
+function receberValores(valor, taxaAoMes, tempo_em_meses){
+
+    // let valor = parseFloat(prompt("Digite o valor Inicial (R$):"))
+    // let taxaAoMes = parseFloat(prompt("Digite a taxa de juros (% ao mes):"))
+    // let tempo_em_meses = parseFloat(prompt("Digite o tempo (em Meses):"))
+    
+    if (isNaN(valor) || isNaN(taxaAoMes) || isNaN(tempo_em_meses) || valor <= 0 || taxaAoMes <= 0 || tempo_em_meses <= 0) {
+        console.log("Por favor, insira valores válidos.");
+        return;
+    }
+
+    // let resultado = calcularJurosComposto(valor, tempo_em_meses, taxaAoMes);
+    // prompt(`O valor final, após ${tempo_em_meses.toFixed(0)} meses, é de ${resultado}`)
+
+    return calcularJurosComposto(valor, taxaAoMes, tempo_em_meses)
+}
+
+receberValores();
+
+module.exports = {calcularJurosComposto, receberValores}
